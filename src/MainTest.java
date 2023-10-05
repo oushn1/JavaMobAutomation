53d21ef1dc06ba31cf99b12cd4c2d7868c700f39
garbuzov@sminex.com <garbuzov@sminex.com>
2023-10-05 15:46:41 +0300

    Initial commit

---
+++ src/MainTest.java
@@ -0,0 +1,26 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainTest
+{
+
+    @Before
+    public void startMessage()
+    {
+
+    }
+
+    @After
+    public void endMessage()
+    {
+
+    }
+
+    @Test
+    public void Test1() {
+
+    }
+
+}
