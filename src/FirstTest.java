bc680bfbdf6c17df7c8adb7f0c4a84d1b2772e8e
Roman Garbuzov <garbuzovromang@gmail.com>
2023-10-17 22:46:52 +0300

    Lesson 2

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,22 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class FirstTest
 {
+
+    public void setUp() throws Exception
+    {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "");
+        capabilities.setCapability("platformVersion", "12");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "C:\\javaqa\\apk");
+    }
 
     @Before
     public void startMessage()
