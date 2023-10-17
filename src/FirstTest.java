df4a9d1d327bc42a129692e8f46cea111af22a24
Roman Garbuzov <garbuzovromang@gmail.com>
2023-10-17 20:18:26 +0300

    Lesson 2

---
+++ src/FirstTest.java
@@ -0,0 +1,25 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FirstTest
+{
+
+    @Before
+    public void startMessage()
+    {
+
+    }
+
+    @After
+    public void endMessage()
+    {
+
+    }
+
+    @Test
+    public void Test1() {
+
+    }
+
+}
