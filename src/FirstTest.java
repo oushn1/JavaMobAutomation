a4a316379c2a9c3744b91ae5c027fe3b2f241f8e
Roman Garbuzov <garbuzovromang@gmail.com>
2023-10-24 20:43:29 +0300

    Lesson 3-2

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -11,6 +11,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FirstTest
 {
@@ -51,6 +53,13 @@ public class FirstTest
         return wait.until(ExpectedConditions.presenceOfElementLocated(by));
     };
 
+    public List<WebElement> findElements(By by, String message)
+    {
+        WebDriverWait wait = new WebDriverWait(this.driver, this.timeout);
+        wait.withMessage(message + "\n");
+        return wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(by));
+    };
+
     public WebElement findElementInElement(By by1, By by2, String message)
     {
         WebDriverWait wait = new WebDriverWait(driver, this.timeout);
@@ -71,13 +80,24 @@ public class FirstTest
     };
 
     @Test
-    public void testSearchFieldContainsText1()
-    {
-        String word_to_search = "reader";
-        By locator_search = By.id("");
+    public void test3() throws InterruptedException {
+        String word_to_search = "docker";
+        By locator_search = By.id("org.wikipedia:id/search_container");
         By locator_clear_search = By.id("org.wikipedia:id/search_close_btn");
-        WebElement search = this.findElement(locator_search, "");
+        By locator_search_result_items = By.id("org.wikipedia:id/page_list_item_title");
+
+        WebElement search = this.findElement(locator_search, "No search field found");
         search.click();
         search.sendKeys(word_to_search);
+        List<WebElement> search_results = this.findElements(locator_search_result_items, "No search results found");
+
+        Assert.assertFalse(search_results.isEmpty());
+        List<WebElement> search_results_cleared = new ArrayList<>();
+        findElement(locator_clear_search, "").click();
+        try {
+            search_results_cleared = this.findElements(locator_search_result_items, "");
+        } catch (Exception exception) {};
+
+        Assert.assertTrue(search_results_cleared.isEmpty());
     };
 }
