622c82c4a79f5fabc99a13a07db4cd2567f3944d
Roman Garbuzov <garbuzovromang@gmail.com>
2023-11-03 18:35:20 +0300

    Lesson 3-2

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,4 +1,6 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Assert;
@@ -11,13 +13,12 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 
 public class FirstTest
 {
     private AppiumDriver driver;
-    public int timeout = 5;
+    public int timeout = 10;
 
     @Before
     public void setUp() throws Exception
@@ -79,25 +80,111 @@ public class FirstTest
         return el_text.contains(expected_text);
     };
 
-    @Test
-    public void test3() throws InterruptedException {
-        String word_to_search = "docker";
+
+    public void FindAndOpenArticle(String title)
+    {
         By locator_search = By.id("org.wikipedia:id/search_container");
-        By locator_clear_search = By.id("org.wikipedia:id/search_close_btn");
         By locator_search_result_items = By.id("org.wikipedia:id/page_list_item_title");
 
         WebElement search = this.findElement(locator_search, "No search field found");
         search.click();
-        search.sendKeys(word_to_search);
+        search.sendKeys(title);
         List<WebElement> search_results = this.findElements(locator_search_result_items, "No search results found");
+        search_results.get(0).click();
+    }
 
-        Assert.assertFalse(search_results.isEmpty());
-        List<WebElement> search_results_cleared = new ArrayList<>();
-        findElement(locator_clear_search, "").click();
-        try {
-            search_results_cleared = this.findElements(locator_search_result_items, "");
-        } catch (Exception exception) {};
+    public void SaveArticleToReadingList()
+    {
+        By locator_save = By.id("Save");
+        By locator_save_to_list = By.id("org.wikipedia:id/snackbar_action");
+        By locator_readings_lists = By.id("org.wikipedia:id/item_title_container");
 
-        Assert.assertTrue(search_results_cleared.isEmpty());
+        WebElement save = this.findElement(locator_save, "Save button not found");
+        save.click();
+        WebElement save_to_list = this.findElement(locator_save_to_list, "Save to list button not found");
+        save_to_list.click();
+        List<WebElement> lists = this.findElements(locator_readings_lists, "");
+        lists.get(0).click();
+    }
+
+    public void RemoveArticleFromReadingList(String article_title, String list_title)
+    {
+        By locator_list = By.xpath("//*[@text='" + list_title + "']");
+        By locator_article = By.xpath("//*[@text='" + article_title + "']");
+
+        WebElement reading_list = this.findElement(locator_list, "Reading list not found");
+        reading_list.click();
+        WebElement article = this.findElement(locator_article, "Article not found");
+        int x = article.getLocation().getX() + 10;
+        int y = article.getLocation().getY() + 10;
+
+        TouchAction swipe = new TouchAction(driver);
+        swipe
+                .press(x+800, y)
+                .waitAction(200)
+                .moveTo(x+100, y)
+                .release()
+                .waitAction(200)
+                .perform();
+    }
+
+    public void openSavedArticle(String article_title)
+    {
+        By locator_article = By.xpath("//*[@text='" + article_title + "']");
+        WebElement article = this.findElement(locator_article, "Article not found");
+        article.click();
+    }
+
+    public void goHome()
+    {
+        By locator_options = By.id("org.wikipedia:id/page_toolbar_button_show_overflow_menu");
+        By locator_option_explore = By.id("org.wikipedia:id/page_explore");
+
+        WebElement options = this.findElement(locator_options, "");
+        options.click();
+        WebElement explore = this.findElement(locator_option_explore, "");
+        explore.click();
+
+    }
+
+    public void goSaved()
+    {
+        By locator_saved = By.id("org.wikipedia:id/nav_tab_reading_lists");
+
+        WebElement saved = this.findElement(locator_saved, "Save button not found");
+        saved.click();
+    }
+
+    @Test
+    public void test41() throws InterruptedException {
+        String text1 = "Plutonium";
+        String text2 = "Uranium";
+        String list_title = "List1";
+
+        //Find and save article 1
+        this.FindAndOpenArticle(text1);
+        this.SaveArticleToReadingList();
+
+        //Find and save article 2
+        this.goHome();
+        this.FindAndOpenArticle(text2);
+        this.SaveArticleToReadingList();
+
+        //Remove article 1
+        this.goHome();
+        this.goSaved();
+        this.RemoveArticleFromReadingList(text1, list_title);
+
+        //Check if article 2 remains = Open article 2
+        this.openSavedArticle(text2);
+
+        //Check if the article has a correct title
+        WebElement article_title = this.findElement(new MobileBy.ByAccessibilityId(text2), "");
+
+        //Remove article 2
+        this.goHome();
+        this.goSaved();
+        this.RemoveArticleFromReadingList(text2, list_title);
+
     };
 }
